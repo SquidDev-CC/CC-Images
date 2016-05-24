4b97eaf19958aea9da1bfe94a81317706be06c75
SquidDev <bonzoweb@hotmail.co.uk>
2016-05-24 21:38:26 +0100

    Add general polygon clipping library
    Woot! Less broken, more massive

--- 3D-Matrix/op/clip.lua
+++ 3D-Matrix/op/clip.lua
@@ -55,9 +55,9 @@ local function transform(vertex, matrix)
 	return trans, clip, projected
 end
 
---- Interpolate between two points
+--- Interpolate between two points and then project them
 -- @tpram matrix ver1 The first vertex
--- @tparam matrix ver2
+-- @tparam matrix ver2 The second vertex
 local function interpolate(ver_1, ver_2, t)
 	local it = 1 - t
 	local newW = ver_1[4] * it + ver_2[4] * t
@@ -70,6 +70,19 @@ local function interpolate(ver_1, ver_2, t)
 	}
 end
 
+--- Interpolate between two points without projecting them
+-- @tpram matrix ver1 The first vertex
+-- @tparam matrix ver2 The second vertex
+local function interpolateNoProject(ver_1, ver_2, t)
+	local it = 1 - t
+	return {
+		ver_1[1] * it + ver_2[1] * t,
+		ver_1[2] * it + ver_2[2] * t,
+		ver_1[3] * it + ver_2[3] * t,
+		ver_1[4] * it + ver_2[4] * t,
+	}
+end
+
 local function pointComplete(matrix, vertex_1, drawPoint, ...)
 	local trans_1, clip_1, proj_1 = transform(vertex_1, matrix)
 
@@ -78,8 +91,9 @@ end
 
 -- Adaptation of https://en.wikipedia.org/wiki/Cohen%E2%80%93Sutherland_algorithm
 -- but using a parametric equation and no loop
-local function trimLine(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2)
-	if clip_1[1] and clip_2[1] then return proj_1, proj_2
+-- also see https://www.cs.unc.edu/xcms/courses/comp770-s07/Lecture07.pdf page 28
+local function line(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2, drawLine, ...)
+	if clip_1[1] and clip_2[1] then return drawLine(proj_1, proj_2, ...)
 	elseif
 		(clip_1[2] ~= 0 and clip_1[2] == clip_2[2]) or
 		(clip_1[3] ~= 0 and clip_1[3] == clip_2[3]) or
@@ -160,13 +174,8 @@ local function trimLine(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2)
 
 		local nproj_1 = interpolate(trans_1, trans_2, tmin)
 		local nproj_2 = interpolate(trans_1, trans_2, tmax)
-		return nproj_1, nproj_2
+		return drawLine(nproj_1, nproj_2, ...)
 	end
-end
-
-local function line(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2, drawLine, ...)
-	proj_1, proj_2 = trimLine(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2)
-	if proj_1 then drawLine(proj_1, proj_2, ...) end
 end
 
 local function lineComplete(matrix, vertex_1, vertex_2, drawLine, ...)
@@ -175,138 +184,223 @@ local function lineComplete(matrix, vertex_1, vertex_2, drawLine, ...)
 	return line(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2, drawLine, ...)
 end
 
-local function rgbD(r, g, b)
-	return math.floor(r * 255) * 256^2 +math.floor(g * 255) * 256 + math.floor(b * 255)
-end
+local function polygon(trans, drawPolygon, ...)
+	local out, n, previous
+	if #trans < 3 then error("Cannot render polygon with < 3 points", 2) end
 
-local function hsv(h, s, v)
-	if s <= 0.0 then
-		return rgbD(v, v, v)
-	end
+	previous, out, n = trans[#trans], {}, 0
+	for i = 1, #trans do
+		local current = trans[i]
+		local cx, cw, px, pw = current[1], current[4], previous[1], previous[4]
 
-	local hh = h * 360;
-	if hh >= 360.0 then hh = 0.0 end
-	hh = hh / 60.0;
-	local i = hh;
-	local ff = hh % 1;
+		if cx >= -cw * 1.00001 then
+			if px < -pw * 1.00001 then
+				-- Previous out, current in
+				local num = -cw - cx
+				local t = num / (num - (-pw - px))
 
-	local p = v * (1.0 - s);
-	local q = v * (1.0 - (s * ff));
-	local t = v * (1.0 - (s * (1.0 - ff)));
+				n = n + 1
+				out[n] = interpolateNoProject(current, previous, t)
+			end
 
-	if i < 1 then
-		return rgbD(v, t, p)
-	elseif i < 2 then
-		return rgbD(q, v, p)
-	elseif i < 3 then
-		return rgbD(p, v, t)
-	elseif i < 4 then
-		return rgbD(p, q, v)
-	elseif i < 5 then
-		return rgbD(t, p, v)
-	else
-		return rgbD(v, p, q)
-	end
-end
-
-local function rneq(a, b)
-	local delta = a - b
-	return delta < -0.00001 or delta > 0.00001
-end
-
-local function req(a, b)
-	local delta = a - b
-	return delta >= -0.00001 and delta <= 0.00001
-end
-
---- Shall we just ignore this
--- This uses the coordinate with the highest magnitude
--- This will probably break at times.
-local function addCorner(proj_1, proj_2, out, n)
-	if not proj_1 or not proj_2 then return n end
-	local x1, y1 = proj_1[1], proj_1[2]
-	local x2, y2 = proj_2[1], proj_2[2]
-
-	if rneq(x1, x2) and rneq(y1, y2) then
-		local mx1 = x1 if mx1 < 0 then mx1 = -mx1 end
-		local mx2 = x2 if mx2 < 0 then mx2 = -mx2 end
-
-		local mx = x1 if mx2 > mx1 then mx = x2 end
-
-		local my1 = y1 if my1 < 0 then my1 = -my1 end
-		local my2 = y2 if my2 < 0 then my2 = -my2 end
-
-		local my = y1 if my2 > my1 then my = y2 end
-
-		n = n + 1
-		out[n] = { mx, my }
-	end
-	return n
-end
-
-local function triangle(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2, trans_3, clip_3, proj_3, drawTriangle, ...)
-	if clip_1[1] and clip_2[1] and clip_3[1] then drawTriangle({proj_1, proj_2, proj_3}, ...) end
-
-	local c1_x, c1_y, c1_z = clip_1[2], clip_1[3], clip_1[4]
-	local c2_x, c2_y, c2_z = clip_2[2], clip_2[3], clip_2[4]
-	local c3_x, c3_y, c3_z = clip_3[2], clip_3[3], clip_3[4]
-
-	if
-		(c1_x ~= 0 and c1_x == c2_x and c1_x == c3_x) or
-		(c1_y ~= 0 and c1_y == c2_y and c1_y == c3_y) or
-		(c1_z ~= 0 and c1_z == c2_z and c1_z == c3_z)
-	then return end
-
-	local proj_1a, proj_1b = trimLine(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2)
-	local proj_2a, proj_2b = trimLine(trans_2, clip_2, proj_2, trans_3, clip_3, proj_3)
-	local proj_3a, proj_3b = trimLine(trans_3, clip_3, proj_3, trans_1, clip_1, proj_1)
-
-	local points, n = {}, 0
-	if proj_1a then
-		points[n + 1] = proj_1a
-		points[n + 2] = proj_1b
-		n = n + 2
-	end
-
-	if proj_2a then
-		local previous = points[n]
-		-- We can use reference identity as it will be the same if not clipped
-		-- and will be different anyway if clipped.
-		if previous ~= proj_2a then
-			n = addCorner(previous, proj_2a, points, n)
 			n = n + 1
-			points[n] = proj_2a
+			out[n] = current
+		elseif px >= -pw * 1.00001 then
+			-- Current out, previous in
+			local num = -pw - px
+			local t = num / (num - (-cw - cx))
+
+			n = n + 1
+			out[n] = interpolateNoProject(previous, current, t)
 		end
 
-		n = n + 1
-		points[n] = proj_2b
+		previous = current
 	end
 
-	if proj_3a then
-		local previous = points[n]
-		if previous ~= proj_3a then
-			n = addCorner(previous, proj_3a, points, n)
+	if n < 3 then return end
+	trans = out
+
+	previous, out, n = trans[#trans], {}, 0
+	for i = 1, #trans do
+		local current = trans[i]
+		local cx, cw, px, pw = current[1], current[4], previous[1], previous[4]
+
+		if cx <= cw * 1.00001 then
+			if px > pw * 1.00001 then
+				-- Previous out, current in
+				local num = cw - cx
+				local t = num / (num - (pw - px))
+
+				n = n + 1
+				out[n] = interpolateNoProject(current, previous, t)
+			end
+
 			n = n + 1
-			points[n] = proj_3a
+			out[n] = current
+		elseif px <= pw * 1.00001 then
+			-- Current out, previous in
+			local num = pw - px
+			local t = num / (num - (cw - cx))
+
+			n = n + 1
+			out[n] = interpolateNoProject(previous, current, t)
 		end
 
-		n = n + 1
-		points[n] = proj_3b
+		previous = current
 	end
 
-	n = addCorner(points[1], points[n], points, n)
-	drawTriangle(points, ...)
+	if n < 3 then return end
+	trans = out
+
+	previous, out, n = trans[#trans], {}, 0
+	for i = 1, #trans do
+		local current = trans[i]
+		local cy, cw, py, pw = current[2], current[4], previous[2], previous[4]
+
+		if cy >= -cw * 1.00001 then
+			if py < -pw * 1.00001 then
+				-- Previous out, current in
+				local num = -cw - cy
+				local t = num / (num - (-pw - py))
+
+				n = n + 1
+				out[n] = interpolateNoProject(current, previous, t)
+			end
+
+			n = n + 1
+			out[n] = current
+		elseif py >= -pw * 1.00001 then
+			-- Current out, previous in
+			local num = -pw - py
+			local t = num / (num - (-cw - cy))
+
+			n = n + 1
+			out[n] = interpolateNoProject(previous, current, t)
+		end
+
+		previous = current
+	end
+
+	if n < 3 then return end
+	trans = out
+
+	previous, out, n = trans[#trans], {}, 0
+	for i = 1, #trans do
+		local current = trans[i]
+		local cy, cw, py, pw = current[2], current[4], previous[2], previous[4]
+
+		if cy <= cw * 1.00001 then
+			if py > pw * 1.00001 then
+				-- Previous out, current in
+				local num = cw - cy
+				local t = num / (num - (pw - py))
+
+				n = n + 1
+				out[n] = interpolateNoProject(current, previous, t)
+			end
+
+			n = n + 1
+			out[n] = current
+		elseif py <= pw * 1.00001 then
+			-- Current out, previous in
+			local num = pw - py
+			local t = num / (num - (cw - cy))
+
+			n = n + 1
+			out[n] = interpolateNoProject(previous, current, t)
+		end
+
+		previous = current
+	end
+
+	if n < 3 then return end
+	trans = out
+
+	previous, out, n = trans[#trans], {}, 0
+	for i = 1, #trans do
+		local current = trans[i]
+		local cz, cw, pz, pw = current[3], current[4], previous[3], previous[4]
+
+		if cz >= -cw * 1.00001 then
+			if pz < -pw * 1.00001 then
+				-- Previous out, current in
+				local num = -cw - cz
+				local t = num / (num - (-pw - pz))
+
+				n = n + 1
+				out[n] = interpolateNoProject(current, previous, t)
+			end
+
+			n = n + 1
+			out[n] = current
+		elseif pz >= -pw * 1.00001 then
+			-- Current out, previous in
+			local num = -pw - pz
+			local t = num / (num - (-cw - cz))
+
+			n = n + 1
+			out[n] = interpolateNoProject(previous, current, t)
+		end
+
+		previous = current
+	end
+
+	if n < 3 then return end
+	trans = out
+
+	previous, out, n = trans[#trans], {}, 0
+	for i = 1, #trans do
+		local current = trans[i]
+		local cz, cw, pz, pw = current[3], current[4], previous[3], previous[4]
+
+		if cz <= cw * 1.00001 then
+			if pz > pw * 1.00001 then
+				-- Previous out, current in
+				local num = cw - cz
+				local t = num / (num - (pw - pz))
+
+				n = n + 1
+				out[n] = interpolateNoProject(current, previous, t)
+			end
+
+			n = n + 1
+			out[n] = current
+		elseif pz <= pw * 1.00001 then
+			-- Current out, previous in
+			local num = pw - pz
+			local t = num / (num - (cw - cz))
+
+			n = n + 1
+			out[n] = interpolateNoProject(previous, current, t)
+		end
+
+		previous = current
+	end
+
+	if n < 3 then return end
+	trans = out
+
+	for i = 1, n do
+		local item = trans[i]
+		local inv = 1 / item[4]
+		item[1] = item[1] * inv
+		item[2] = item[2] * inv
+		item[3] = item[3] * inv
+	end
+
+	drawPolygon(trans, ...)
 end
 
-local function triangleComplete(matrix, vertex_1, vertex_2, vertex_3, drawTriangle, ...)
-	local trans_1, clip_1, proj_1 = transform(vertex_1, matrix)
-	local trans_2, clip_2, proj_2 = transform(vertex_2, matrix)
-	local trans_3, clip_3, proj_3 = transform(vertex_3, matrix)
-	return triangle(trans_1, clip_1, proj_1, trans_2, clip_2, proj_2, trans_3, clip_3, proj_3, drawTriangle, ...)
+local function polygonComplete(matrix, vertex, drawPolygon, ...)
+	local out = {}
+	for i = 1, #vertex do
+		out[i] = vector(matrix, vertex[i])
+	end
+	return polygon(out, drawPolygon, ...)
 end
 
 return {
 	point = pointComplete,
 	line = lineComplete,
-	triangle = triangleComplete,
+	polygon = polygonComplete,
 }
--- 3D-Matrix/op/main.lua
+++ 3D-Matrix/op/main.lua
@@ -110,71 +110,6 @@ local player = sensor.getPlayerByName("ThatVeggie") or error("Cannot find")
 
 local initialPosition = { x = 0.5, y = 0.5, z = 0.5 }
 
-
-local verticies = {
-	{  3,  3, -3, 1 },
-	{  3, -3, -3, 1 },
-	{ -3, -3, -3, 1 },
-	{ -3,  3, -3, 1 },
-	{  3,  3,  3, 1 },
-	{  3, -3,  3, 1 },
-	{ -3, -3,  3, 1 },
-	{ -3,  3,  3, 1 },
-}
-
-for i = 1, #verticies do
-	verticies[i][2] = verticies[i][2] - 5
-end
-
-local colours = {
-	{ 255, 0,   0,   255 },
-	{ 0,   255, 0,   255 },
-	{ 0,   0,   255, 255 },
-	{ 0,   255, 255, 255 },
-	{ 255, 255, 0,   255 },
-	{ 255, 0,   255, 255 },
-	{ 255, 128, 0,   255 },
-	{ 128, 255, 0,   255 },
-}
-
-for i = 1, #colours do colours[i] = rgb(unpack(colours[i])) end
-
-local indexes = {
-	{1,2,3, 1},
-	{1,3,4, 1},
-	{7,6,8, 2},
-	{8,6,5, 2},
-
-	{3,7,4, 3}, -- Left
-	{4,7,8, 3},
-	{5,6,2, 4},
-	{2,1,5, 4},
-
-	{5,1,4, 5}, -- Top
-	{4,8,5, 5},
-	{2,6,3, 6},
-	{6,7,3, 6},
-}
-
-for i = 1, #indexes do
-	local tri = indexes[i]
-
-	local a = verticies[tri[1]]
-	local b = verticies[tri[2]]
-	local c = verticies[tri[3]]
-
-	tri[5] = {
-		(a[1] + b[1] + c[1]) / 3,
-		(a[2] + b[2] + c[2]) / 3,
-		(a[3] + b[3] + c[3]) / 3,
-	}
-end
-
-
-local function sortDistance(a, b)
-	return a[6] >= b[6]
-end
-
 local counter = 0
 while true do
 	counter = (counter + 0.005) % 1
@@ -196,51 +131,17 @@ while true do
 	local mvp = compose(projection, view)
 
 	graphics.clear()
-	-- byzanz-record --duration=15 --x=200 --y=300 --width=700 --height=400 out.gif
-	--
-	-- clip.line(
-	-- 	mvp,
-	-- 	{ -2, 0, 0, 1 },
-	-- 	{ -2, 0, 4, 1 },
-	-- 	drawLine,
-	-- 	hsv(counter, 0.6, 1), 0.25, 3
-	-- )
-	--
-	-- clip.line(
-	-- 	mvp,
-	-- 	{ -2, 0, 0, 1 },
-	-- 	{ -2, 0, 4, 1 },
-	-- 	drawLine,
-	-- 	hsv(counter, 0.6, 1), 1, 1
-	-- )
 
-	clip.triangle(
+	clip.polygon(
 		mvp,
-		{ 0, 0, 0, 1 },
-		{ 0, 0, 3, 1 },
-		{ 3, 0, 0, 1 },
+		{
+			{ 0, 0, 0, 1 },
+			{ 0, 0, 3, 1 },
+			{ 3, 0, 0, 1 }
+		},
 		drawTriangle,
 		hsv(counter, 0.6, 1), 0.5
 	)
-
-	for i = 1, #indexes do
-		local tri = indexes[i]
-		local mid = tri[5]
-		tri[6] = (mid[1] - x)^2 + (mid[2] - y)^2 + (mid[3] - z)^2
-	end
-	table.sort(indexes, sortDistance)
-	for i = 1, #indexes do
-		local tri = indexes[i]
-
-		clip.triangle(
-			mvp,
-			verticies[tri[1]],
-			verticies[tri[2]],
-			verticies[tri[3]],
-			drawTriangle,
-			colours[tri[4]], 0.6
-		)
-	end
 
 	graphics.addText(5, 5, (textutils.serialize(pData.living.lookingAt):gsub("%s+", " ")))
 	graphics.addText(5, 15, (textutils.serialize(pData.position):gsub("%s+", " ")))
